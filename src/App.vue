9ed1f06f86a17131e9ea68fc67f8ca356dd59a69
Aya <squallihoussainiaya@gmail.com>
2025-04-28 12:29:58 +0100

    add supabase for image storage

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,17 @@
   <router-view></router-view>
 </template>
 
+<script>
+import { supabase } from "./supabase";
+export default {
+  provide(){
+    return {
+      supabase,
+    };
+  },
+};
+</script>
+
 <style>
 *{
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
